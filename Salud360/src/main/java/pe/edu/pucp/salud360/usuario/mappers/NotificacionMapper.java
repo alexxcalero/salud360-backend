55333c6982158724de8213f41651bf229b97ca26
Fabián <fabianmr2806@hotmail.com>
2025-06-02 17:58:36 -0500

    Notificaciones

--- Salud360/src/main/java/pe/edu/pucp/salud360/usuario/mappers/NotificacionMapper.java
+++ Salud360/src/main/java/pe/edu/pucp/salud360/usuario/mappers/NotificacionMapper.java
@@ -1,10 +1,12 @@
 package pe.edu.pucp.salud360.usuario.mappers;
 
 import org.mapstruct.Mapper;
+import pe.edu.pucp.salud360.servicio.mappers.ReservaMapper;
+import pe.edu.pucp.salud360.servicio.mappers.ServicioMapper;
 import pe.edu.pucp.salud360.usuario.dtos.NotificacionDTO;
 import pe.edu.pucp.salud360.usuario.models.Notificacion;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = {ClienteMapper.class, ReservaMapper.class})
 public interface NotificacionMapper {
 
     NotificacionDTO mapToDTO(Notificacion notificacion);
