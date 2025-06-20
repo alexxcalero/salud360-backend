4c2fc576440b84751d66518ca5533fc7d4e2510f
Piero Pastor <102126264+PieroPastor@users.noreply.github.com>
2025-06-19 22:19:42 -0500

    fefdfsdfa

--- Salud360/src/main/java/pe/edu/pucp/salud360/principal/WebConfig.java
+++ Salud360/src/main/java/pe/edu/pucp/salud360/principal/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("http://localhost:5173", "http://ec2-54-91-52-110.compute-1.amazonaws.com:5173")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
