d537fa941af527f44dc16af54bc2548f146f820c
iGorillaVR <131416317+iGorillaVR@users.noreply.github.com>
2025-06-28 23:58:02 -0500

    CARGA MASIVA LOCAL

--- Salud360/src/main/java/pe/edu/pucp/salud360/servicio/services/servicesImp/LocalServiceImp.java
+++ Salud360/src/main/java/pe/edu/pucp/salud360/servicio/services/servicesImp/LocalServiceImp.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 import pe.edu.pucp.salud360.servicio.dto.LocalDTO.LocalDTO;
 import pe.edu.pucp.salud360.servicio.dto.LocalDTO.LocalVistaAdminDTO;
 import pe.edu.pucp.salud360.servicio.mappers.LocalMapper;
@@ -13,10 +15,19 @@ import pe.edu.pucp.salud360.servicio.repositories.LocalRepository;
 import pe.edu.pucp.salud360.servicio.repositories.ServicioRepository;
 import pe.edu.pucp.salud360.servicio.services.LocalService;
 
+
+
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
+//Para la carga masiva
+import java.io.InputStream;
+import com.univocity.parsers.csv.CsvParserSettings;
+import com.univocity.parsers.csv.CsvParser;
+import com.univocity.parsers.common.record.Record;
+import java.io.IOException;
 
 @Service
 public class LocalServiceImp implements LocalService {
@@ -121,6 +132,43 @@ public class LocalServiceImp implements LocalService {
         return localRepository.findById(id)
                 .map(localMapper::mapToVistaAdminDTO)
                 .orElse(null);
+    }
+
+    @Override
+    @Transactional
+    public Boolean cargarMasivamante(MultipartFile file) throws IOException {
+        //LOGICA DEL VIDEO
+        List<Local> listaLocales= new ArrayList<>();
+        InputStream inputStream = file.getInputStream();
+        CsvParserSettings settings = new CsvParserSettings();
+        settings.setHeaderExtractionEnabled(true);
+        CsvParser parser = new CsvParser(settings);
+        List<Record> parseAllRecords= parser.parseAllRecords(inputStream);
+        parseAllRecords.forEach(record -> {
+            Local local = new Local();
+            //Lectura de los parametros que aparecen en el CSV
+            //nombre,direccion,telefono,tipo_servicio,id_servicio,descripcion
+            local.setNombre(record.getString("nombre"));
+            local.setDireccion(record.getString("direccion"));
+            local.setTelefono(record.getString("telefono"));
+            local.setTipoServicio(record.getString("tipo_servicio"));
+            // COMO TENEMOS QUE ASOCIAR UN ID DE UN SERVICIO EXISTENTE, LO BUSCAMOS
+            Integer idServicio = Integer.parseInt(record.getString("id_servicio"));
+            Servicio servicio = servicioRepository.findById(idServicio)
+                    .orElseThrow(() -> new RuntimeException("Servicio con ID " + idServicio + " no encontrado"));
+            local.setServicio(servicio);
+
+            local.setDescripcion(record.getString("descripcion"));
+
+            //Datos crudos que debemos insertar
+            local.setActivo(true);
+            local.setFechaCreacion(LocalDateTime.now());
+            //Agregamos el local
+            listaLocales.add(local);
+        });
+        //El safeAll
+        localRepository.saveAll(listaLocales);
+        return true;
     }
 
 }
